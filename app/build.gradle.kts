0abd9eb05c32430e2557824e53fd757320154d77
Patrick Linton <patrickdaniellinton@gmail.com>
2026-09-08 14:20:30 +0100

    build: version 0.2
    Android will only install an update over an existing copy when the
    version code increases, so this has to move for the v0.2.0 release to be
    installable over v0.1.0.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "org.pashri.bustimes"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1"
+        versionCode = 2
+        versionName = "0.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
