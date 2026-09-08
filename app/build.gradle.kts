c4c7363ee862ede1f94206cc4dfda73c8c3e06f9
Patrick Linton <patrickdaniellinton@gmail.com>
2026-09-08 18:54:21 +0100

    build: version 0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "org.pashri.bustimes"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.4"
+        versionCode = 5
+        versionName = "0.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
