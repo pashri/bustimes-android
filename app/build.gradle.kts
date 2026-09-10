ad417145ef993297d89cf7e7c29caefa1892f68a
Patrick Linton <patrickdaniellinton@gmail.com>
2026-09-10 15:01:26 +0100

    [#6] chore: bump to 0.7 for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "org.pashri.bustimes"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.6"
+        versionCode = 7
+        versionName = "0.7"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
