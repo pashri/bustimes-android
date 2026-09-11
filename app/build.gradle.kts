c34880dea8e6678e27e5d4f6e9a3bfaf39bf3248
Patrick Linton <patrickdaniellinton@gmail.com>
2026-09-11 22:11:36 +0100

    [#20] chore: bump to 0.10 for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "org.pashri.bustimes"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "0.9"
+        versionCode = 10
+        versionName = "0.10"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
